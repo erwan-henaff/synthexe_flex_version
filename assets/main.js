d4b0f979a9ba42e8255f0f9ce0adf283a9341c4d
erwan-henaff <erwan.in.berlin@gmx.de>
2019-06-17 12:02:50 +0200

    added animation on scroll with main.js file, added section sequencer, added footer

---
+++ assets/main.js
@@ -0,0 +1,33 @@
+// from : https://eddyerburgh.me/animate-elements-scrolled-view-vanilla-js
+
+
+var animateonscroll = function () {
+    var verticallength;
+    var rowtoanimate;
+
+    function init () {
+        rowtoanimate = document.querySelectorAll(".notinview");
+        verticallength = window.innerHeight;
+        eventhandler();
+        checkposition();
+    }
+    function eventhandler () {
+        window.addEventListener('scroll', checkposition);
+        window.addEventListener('resize', init);
+    }
+    function checkposition () {
+        for (let index = 0; index < rowtoanimate.length; index++) {
+            var actualposition = rowtoanimate[index].getBoundingClientRect().top;
+            if (actualposition - verticallength <= 0) {
+                rowtoanimate[index].className = rowtoanimate[index].className.replace('notinview','rotatez');
+            } 
+            else {
+                rowtoanimate[index].className = rowtoanimate[index].className.replace('rotatez','notinview');
+            }
+        }
+    }
+    return {
+        init : init
+    };    
+};
+animateonscroll().init();
