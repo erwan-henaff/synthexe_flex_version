73f29f3e5c25e7bd694c0662d846260f230a6701
erwan-henaff <erwan.in.berlin@gmx.de>
2019-06-18 16:09:19 +0200

    failed  javascript attempt on creating a carousel

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,6 @@
 // from : https://eddyerburgh.me/animate-elements-scrolled-view-vanilla-js
 // rewritten and slightly changed.
 
-
 var animateonscroll = function () {
     var verticallength;
     var rowtoanimate;
@@ -32,3 +31,25 @@ var animateonscroll = function () {
     };    
 };
 animateonscroll().init();
+
+// part about the carousel with vanilla js 
+// from https://www.youtube.com/watch?v=KcdBOoK3Pfw
+
+var imgcarousel = document.querySelectorAll('.imgcarousel');
+
+var rightbtn = document.querySelector('#rightbtn');
+
+rightbtn.addEventListener('click', moveImgRight);
+
+var positotheright = imgcarousel[0].style.left;
+
+var movingunit = window.innerWidth * 80/100;
+
+function moveImgRight () {
+    for (let index = 0; index < imgcarousel.length; index++) {
+        var movetorightdist = parseInt(imgcarousel[index].style.left);
+        movetorightdist = movetorightdist + movingunit; 
+        imgcarousel[index].style.left = movetorightdist + "px";
+    }
+    
+};
